c52d34a79241f27fa60fc96cc961965b821ba471
IgnacioCG28 <ignaciocarmonagonzalez@alumnos.tuniversformacion.es>
2023-11-15 11:43:26 +0100

    clases

---
+++ pizza.php
@@ -0,0 +1,9 @@
+<?php
+class Pizza extends Articulo {
+    public $ingredientes;
+
+    public function __construct($nombre, $coste, $precio, $contador, $ingredientes) {
+        parent::__construct($nombre, $coste, $precio, $contador);
+        $this->ingredientes = $ingredientes;
+    }
+}
